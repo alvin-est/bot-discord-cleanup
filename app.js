1f510b360f3fda893031898ef2e3b8933b18cb4d
alvin-est <estado.alvin@gmail.com>
2025-01-19 22:27:28 +1100

    implemented main bot functionality

--- app.js
+++ app.js
@@ -2,7 +2,78 @@
 // Module to safely handle Discord's token functionality
 require('dotenv').config(); 
 
-const { Client, GatewayIntentBits, Partials } = require('discord.js');
+
+// Bot config (use ENV)
+
+// Development Mode: If true, messages are deleted every minute
+const dev_mode = process.env.DEV_MODE;
+
+// Purge Age: Delete messages after 'x' amount of hours
+const purge_age_hours = process.env.PURGE_AGE;
+
+/* Determines message age before auto-deletion */ 
+function purgeAgeInMs() {
+    if (dev_mode) {
+        return 60000; // Delete messages after 60 seconds
+    }
+
+    if(!purge_age_hours) {
+        throw new Error('purge_age error. please input amount of hours before message deletion.');
+    }
+
+    const ms_in_hour = 60 * 60 * 1000;
+    const total_ms = purge_age_hours * ms_in_hour;
+
+    return total_ms;
+}
+
+function printTimer() {
+    if(dev_mode === 'true' || dev_mode === 'TRUE') {
+        return '60s';
+    }
+    if(dev_mode === 'false' || dev_mode === 'FALSE') {
+        return `${purge_age_hours}h`;
+    }
+}
+
+/* Announce bot activation in specified auto-delete channel */
+function onAliveAnnounce() {
+    const channelId = process.env.CHANNEL_ID;
+    const channel = client.channels.cache.get(channelId);
+
+    if (channel && channel.isTextBased()) {
+        channel.send('Bot is now online!');
+        channel.send('Cleanup crew activated.');
+        if(dev_mode === 'false' || dev_mode === 'FALSE'){ 
+            channel.send(`Messages will auto-delete after ${purge_age_hours} hours.`);
+        }
+        else if (dev_mode === 'true' || dev_mode === 'TRUE') { 
+            channel.send('Development mode is on.');
+            channel.send('Messages will be deleted after 1 minute.');
+        }
+        else {
+            channel.send('Configuration error. Shutting down!');
+            throw new Error('dev_mode error. value not set to true or false.');
+        }
+    } else {
+        console.error('Channel not found or not text-based.');
+    }
+}
+
+function onAliveSetStatus() {
+    client.user.setPresence({
+        activities: [{
+            name: "the time..", // You can set a custom status message here
+            type: ActivityType.Watching // Can be one of: Playing, Streaming, Listening, Watching, Competing
+        }],
+        status: 'online' // online, idle, dnd, or invisible
+    });
+
+    client.user.setActivity(`Auto-delete timer: ${printTimer()}`, { type: ActivityType.Custom });
+}
+
+/* Discord.JS code below */
+const { Client, GatewayIntentBits, Partials, ActivityType } = require('discord.js');
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
@@ -12,6 +83,7 @@ const client = new Client({
     partials: [Partials.Channel]
 });
 
+
 // Store messages to be deleted
 const messagesToDelete = new Map();
 
@@ -20,13 +92,15 @@ const CHANNEL_ID = process.env.CHANNEL_ID;
 
 client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}`);
-    setInterval(deleteOldMessages, 3600000); // Check every hour (3600000 ms)
+    setInterval(deleteOldMessages, 60000); // Check every minute (60000 ms)
+    onAliveAnnounce(); // Announce bot presence in channel
+    onAliveSetStatus();
 });
 
 client.on('messageCreate', message => {
     if (message.guild && message.channelId === CHANNEL_ID) {
         // Store message id with the timestamp when it should be deleted
-        messagesToDelete.set(message.id, message.createdTimestamp + 86400000); // 86400 seconds in a day * 1000 for ms
+        messagesToDelete.set(message.id, message.createdTimestamp + purgeAgeInMs()); // 60 seconds * 1000 for ms
     }
 });
 
@@ -55,5 +129,5 @@ function deleteOldMessages() {
     });
 }
 
-// Replace 'YOUR_BOT_TOKEN' with the actual token from Discord Developer Portal
+// Bot token from Discord Developer Portal
 client.login(process.env.BOT_TOKEN);
